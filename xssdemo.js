b24c48e96d1697dcfecf89cf964f6d4d82e4f725
Lisa Li <xinleili126@gmail.com>
2018-03-15 00:02:30 -0400

    Update xssdemo.js

--- xssdemo.js
+++ xssdemo.js
@@ -1,5 +1,5 @@
 
-cess.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var request = require('request');
 var fs = require('fs');
 var http = require('http');
